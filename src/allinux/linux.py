151d060de81a01834fabb21a9da2d48cb6e019a4
Andrew Li <andrewliqw@outlook.com>
2017-04-06 02:14:55 +0000

    add cd command

--- src/allinux/linux.py
+++ src/allinux/linux.py
@@ -6,6 +6,14 @@ import os
 import shutil
 
 
+def cd(path=None):
+    if path:
+        os.chdir(path)
+    else:
+        home_dir = os.environ['HOME']
+        os.chdir(home_dir)
+
+
 def cp(src, dst, *, follow_symlinks=True):
     shutil.copy(src, dst, follow_symlinks=follow_symlinks)
 
