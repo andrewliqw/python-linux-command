e3286fdbae6cf19a03a577a202343154d79a480d
Andrew Li <andrewliqw@outlook.com>
2017-04-19 03:20:28 +0000

    change version to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name='allinux',
     
-    version='0.1.0',
+    version='0.2.0',
     
     description='Linux command collections',
     long_description='Linux command collections',
